b63eadd2378d74392fb9609aef5ef9bec0126245
jeremy <jbarnesspain@gmail.com>
2021-04-20 15:44:29 +0200

    added script to use mtool to convert sentiment jsons to mrp format

---
+++ convert_sentiment_data_to_mrp_format.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for dataset in ca ds_services ds_unis en es eu mpqa norec_fine; do
+    for split in train dev test; do
+        indata=data/"$dataset"/"$split".json;
+        outdata=data/"$dataset"/"$split".mrp;
+        python3 mtool/main.py --reify --strings --ids --read norec --write mrp "$indata" "$outdata"
+    done;
+done;
