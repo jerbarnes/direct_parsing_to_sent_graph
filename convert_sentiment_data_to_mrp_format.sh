360c5b1af76b438c41c6700895a46bdc01dafe3c
davda54 <david.samuel@seznam.cz>
2021-09-29 13:19:25 +0200

    add coverted datasets

--- convert_sentiment_data_to_mrp_format.sh
+++ convert_sentiment_data_to_mrp_format.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for dataset in ca ds_services ds_unis en es eu mpqa norec_fine; do
+for dataset in darmstadt_unis mpqa multibooked_ca multibooked_eu norec opener_en opener_es; do
+    mkdir data/labeled_edge_mrp/"$dataset"
+    mkdir data/node_centric_mrp/"$dataset"
     for split in train dev test; do
-        indata=data/"$dataset"/"$split".json;
-        outdata=data/"$dataset"/"$split".mrp;
+        indata=data/raw/"$dataset"/"$split".json;
+
+        outdata_edge=data/labeled_edge_mrp/"$dataset"/"$split".mrp;
+        outdata_node=data/node_centric_mrp/"$dataset"/"$split".mrp;
+
         #python3 mtool/main.py --reify --strings --ids --read norec --write mrp "$indata" "$outdata"
-        #python3 mtool/main.py --node_centric --strings --ids --read norec --write mrp "$indata" "$outdata"
-        python3 mtool/main.py --reify --strict --strings --ids --read norec --write mrp "$indata" "$outdata"
+        python3 mtool/main.py --node_centric --strings --ids --read norec --write mrp "$indata" "$outdata_node"
+        python3 mtool/main.py --reify --strict --strings --ids --read norec --write mrp "$indata" "$outdata_edge"
     done;
 done;
