transformers gradient checkpointing fix

perin/model/head/abstract_head.py
@@ -48,7 +48,7 @@ class AbstractHead(nn.Module):
         decoder_lens = self.query_length * word_lens
         batch_size = every_input.size(0)
 
-        label_pred = self.forward_label(decoder_output, decoder_lens)
+        label_pred = self.forward_label(decoder_output)
         anchor_pred = self.forward_anchor(decoder_output, encoder_output, encoder_mask, mode="anchor")  # shape: (B, T_l, T_w)
         source_anchor_pred = self.forward_anchor(decoder_output, encoder_output, encoder_mask, mode="source_anchor")  # shape: (B, T_l, T_w)
         target_anchor_pred = self.forward_anchor(decoder_output, encoder_output, encoder_mask, mode="target_anchor")  # shape: (B, T_l, T_w)
@@ -134,7 +134,7 @@ class AbstractHead(nn.Module):
         losses.update(self.loss_edge_label(output, batch, edge_label_mask.unsqueeze(-1)))
 
         stats = {f"{key}": value.detach().cpu().item() for key, value in losses.items()}
-        total_loss = sum(losses.values())
+        total_loss = sum(losses.values()) / len(losses)
 
         return total_loss, stats
 

perin/model/module/encoder.py
@@ -9,7 +9,6 @@ import torch.nn.functional as F
 
 from transformers.models.bert import BertModel
 from transformers.models.xlm_roberta import XLMRobertaModel
-from transformers import RobertaConfig
 from model.module.char_embedding import CharEmbedding
 
 
@@ -33,15 +32,14 @@ class Encoder(nn.Module):
         self.width_factor = args.query_length
 
         if "roberta" in args.encoder.lower():
-            config = RobertaConfig.from_pretrained(args.encoder)
-            config.update({"gradient_checkpointing": True})
-            self.bert = XLMRobertaModel(config, add_pooling_layer=False)
+            self.bert = XLMRobertaModel.from_pretrained(args.encoder, add_pooling_layer=False)
+            self.bert._set_gradient_checkpointing(self.bert.encoder, value=True)
             if args.encoder_freeze_embedding:
                 self.bert.embeddings.requires_grad_(False)
                 self.bert.embeddings.LayerNorm.requires_grad_(True)
         else:
             self.bert = BertModel.from_pretrained(args.encoder, gradient_checkpointing=True, add_pooling_layer=False)
-        
+
         if args.freeze_bert:
             self.bert.requires_grad_(False)
 

perin/utility/autoclip.py
@@ -7,8 +7,8 @@ import torch.nn as nn
 
 class AutoClip:
     def __init__(self, parameters, initial_clipping=0.1, percentile=50, history_len=1000):
-        self.parameters = parameters
-        self.grad_history = [torch.full([history_len], initial_clipping) for _ in parameters]
+        self.parameters = list(parameters)
+        self.grad_history = [torch.full([history_len], initial_clipping) for _ in self.parameters]
 
         self.index = 0
         self.history_len = history_len