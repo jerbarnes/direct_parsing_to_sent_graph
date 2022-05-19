f19379fb038df55a152d9d0867c1a35b39e3b06c
davda54 <david.samuel@seznam.cz>
2022-05-19 14:52:26 +0200

    fixed inference code

--- perin/inference.py
+++ perin/inference.py
@@ -15,11 +15,11 @@ from utility.predict import predict
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--checkpoint", type=str, required=True)
-    parser.add_argument("--data_directory", type=str, default="/home/samueld/mrp_update/mrp")
+    parser.add_argument("--data_directory", type=str, default="../data")
     args = parser.parse_args()
 
     checkpoint = torch.load(args.checkpoint)
-    args = Params().load_state_dict(checkpoint["args"])
+    args = Params().load_state_dict(checkpoint["params"])
     args.log_wandb = False
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -33,4 +33,4 @@ if __name__ == "__main__":
     directory = "./inference_prediction"
     os.makedirs(directory, exist_ok=True)
     print("inference of validation data", flush=True)
-    predict(model, dataset.val, args.validation_data, args, None, directory, device)
+    predict(model, dataset.val, args.validation_data, args.raw_validation_data, args, None, directory, device)
